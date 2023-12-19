c124c017ce0352cb6f9ca2f37e201a2d2d2cc3d0
Weile <wzheng13@u.rochester.edu>
2023-12-19 15:29:25 -0500

    Update minor bugs

--- rvf-web/client/src/Result.tsx
+++ rvf-web/client/src/Result.tsx
@@ -15,7 +15,7 @@ const Result = ({ result }: ResultProps) => {
 		<>
 			<div id="outlined-box">
 				Model Result
-				<p>Image Procesing Complete</p>
+				{Object.keys(finalObject).length !== 0 && <p>Image Processing Complete</p>}
 				<p>{finalObject.result}</p>
 				<p>GradCam</p>
 				<img src={finalObject.gradCamPath} height="250" width="250" />
